bd1112549fd8cde62a0062abdf22ed10faebe81e
Faiza-Mazhar <faiza.mazhar@bbc.co.uk>
2020-07-30 09:02:31 +0100

    Change return data type

--- app/src/main/java/com/example/quiztrivia/dataservice/DataHandlerRetroFit.kt
+++ app/src/main/java/com/example/quiztrivia/dataservice/DataHandlerRetroFit.kt
@@ -1,10 +1,10 @@
 package com.example.quiztrivia.dataservice
 
-import com.example.quiztrivia.optionselection.QuestionDefinition
+import com.example.quiztrivia.optionselection.QuestionsDefinitionList
 
 class DataHandlerRetroFit(): DataHandler {
 
-    override fun getQuestionDefinition(urlString: String): List<QuestionDefinition> {
+    override fun getQuestionDefinition(urlString: String): QuestionsDefinitionList {
         TODO("Not yet implemented")
     }
 }
